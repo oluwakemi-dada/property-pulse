d3349406707e84fddd8dbb008c59295fb9fd2884
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-08-06 21:42:17 +0100

    feat: properties page

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,5 +1,24 @@
+import properties from '@/properties.json';
+import PropertyCard from '@/components/PropertyCard';
+
 const PropertiesPage = () => {
-  return <div>Properties Page</div>;
+  return (
+    <>
+      <section className="px-4 py-6">
+        <div className="m-auto px-4 py-6 lg:container">
+          {properties.length === 0 ? (
+            <p>No properties found</p>
+          ) : (
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+              {properties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default PropertiesPage;
