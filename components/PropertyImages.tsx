09f25512c3c550e2e56cb8986a3d52ae024d3823
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-08-15 16:28:33 +0100

    fix: lint fix

--- components/PropertyImages.tsx
+++ components/PropertyImages.tsx
@@ -7,60 +7,53 @@ type PropertyImagesProps = {
   images: string[];
 };
 
+function PsItem({ src, className }: { src: string; className: string }) {
+  const node = (
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore - react-photoswipe-gallery Item typing not aligned with React 19
+    <Item original={src} thumbnail={src} width={1000} height={600}>
+      {({ ref, open }) => (
+        <Image
+          ref={ref}
+          onClick={open}
+          src={src}
+          alt=""
+          width={1800}
+          height={400}
+          priority
+          className={className}
+        />
+      )}
+    </Item>
+  );
+  return node;
+}
+
 const PropertyImages = ({ images }: PropertyImagesProps) => {
   return (
     <Gallery>
       <section className="bg-blue-50 p-4">
         <div className="container mx-auto">
           {images.length === 1 ? (
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            <Item
-              original={images[0]}
-              thumbnail={images[0]}
-              width={1000}
-              height={600}
-            >
-              {({ ref, open }) => (
-                <Image
-                  ref={ref}
-                  onClick={open}
-                  src={images[0]}
-                  alt=""
-                  width={1800}
-                  height={400}
-                  priority={true}
-                  className="mx-auto h-[400px] cursor-pointer rounded-xl object-cover"
-                />
-              )}
-            </Item>
+            <PsItem
+              src={images[0]}
+              className="mx-auto h-[400px] cursor-pointer rounded-xl object-cover"
+            />
           ) : (
             <div className="grid grid-cols-2 gap-4">
               {images.map((image, index) => (
                 <div
                   key={index}
-                  className={`${images.length === 3 && index === 2 ? 'col-span-2' : 'col-span-1'}`}
+                  className={
+                    images.length === 3 && index === 2
+                      ? 'col-span-2'
+                      : 'col-span-1'
+                  }
                 >
-                  {/* @ts-expect-error - PhotoSwipe Item type mismatch in react-photoswipe-gallery */}
-                  <Item
-                    original={image}
-                    thumbnail={image}
-                    width={1000}
-                    height={600}
-                  >
-                    {({ ref, open }) => (
-                      <Image
-                        ref={ref}
-                        onClick={open}
-                        src={image}
-                        alt=""
-                        width={1800}
-                        height={400}
-                        priority={true}
-                        className="h-[400px] w-full cursor-pointer rounded-xl object-cover"
-                      />
-                    )}
-                  </Item>
+                  <PsItem
+                    src={image}
+                    className="h-[400px] w-full cursor-pointer rounded-xl object-cover"
+                  />
                 </div>
               ))}
             </div>
