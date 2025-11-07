99bc6ad8707bf159d21700371d18620a3d12d5dc
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-11-07 19:32:34 +0100

    fix: fix lint errors

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -10,10 +10,14 @@ let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === "development") {
   // Prevent multiple connections in dev
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   if (!(global as any)._mongoClientPromise) {
     client = new MongoClient(uri, options);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (global as any)._mongoClientPromise = client.connect();
   }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   clientPromise = (global as any)._mongoClientPromise;
 } else {
   client = new MongoClient(uri, options);
