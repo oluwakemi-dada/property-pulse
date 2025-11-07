53f8012a3b8d49466f8848131b8eb4720f8dfe4a
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-11-07 19:10:27 +0100

    fix: fix login issues

--- auth.ts
+++ auth.ts
@@ -1,10 +1,11 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
-import connectDB from '@/config/database';
-import User from '@/models/User';
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
 import { authConfig } from './auth.config';
 
 export const { auth, signIn, signOut, handlers } = NextAuth({
+  adapter: MongoDBAdapter(clientPromise), 
   ...authConfig, // spread the minimal config (including authorized callback)
   providers: [
     Google({
@@ -21,39 +22,12 @@ export const { auth, signIn, signOut, handlers } = NextAuth({
   ],
   callbacks: {
     ...authConfig.callbacks,
-    // Invoked on successful sign in
-    async signIn({ profile }): Promise<boolean> {
-      // 1. Connect to the database
-      await connectDB();
-      // 2. Check if user exists
-      const userExists = await User.findOne({ email: profile?.email });
-      // 3. If not, create user
-      if (!userExists) {
-        // Truncate username if too long
-        const username = profile?.name?.slice(0, 20);
 
-        if (!profile) return false;
-
-        console.log(profile);
-
-        await User.create({
-          email: profile?.email,
-          username,
-          image: profile.picture,
-        });
-      }
-      // 4. Return true to allow sign in
-      return true;
-    },
-    // Session cb function that modifies the session object
-    async session({ session }) {
+    async session({ session, user }) {
       if (!session.user) return session;
 
-      // 1. Get user from database
-      const user = await User.findOne({ email: session?.user?.email });
-      // 2. Assign user if from the session
-      session.user.id = user._id.toString();
-      // 3. Return the session
+      session.user.id = user.id; // user.id comes from the adapter
+    
       return session;
     },
   },
---
+++ lib/mongodb.ts
@@ -0,0 +1,23 @@
+import { MongoClient } from "mongodb";
+
+if (!process.env.MONGODB_URI) throw new Error("MONGODB_URI not defined");
+
+const uri = process.env.MONGODB_URI;
+const options = {};
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === "development") {
+  // Prevent multiple connections in dev
+  if (!(global as any)._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    (global as any)._mongoClientPromise = client.connect();
+  }
+  clientPromise = (global as any)._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
